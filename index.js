511e2379679492271e2453883461ca623a3cbfbf
Jonathen Whitford <paredes.jonathen@yahoo.com>
2024-07-10 15:54:48 -0400

    Completed Lab

---
+++ index.js
@@ -0,0 +1,44 @@
+function add(a, b) { 
+    return a + b 
+}
+add(834, 835)
+
+function subtract(a, b) {
+    return a - b
+}
+subtract(734, 1)
+
+function multiply(a, b){
+    return a * b
+}
+multiply(139780, 2)
+
+function divide(a, b){
+    return a / b
+}
+divide(3, 2)
+
+function increment(n) {
+    return n+1
+}
+
+function decrement(x) {
+    x--
+    return x
+}
+
+function makeInt(str) {
+    return parseInt(str);
+  }
+
+ function makeInt(str) { 
+    return parseFloat(str)
+ }
+
+ function preserveDecimal(str){
+    return parseInt(str)
+ }
+
+ function preserveDecimal(str) {
+    return parseFloat (str)
+ }
